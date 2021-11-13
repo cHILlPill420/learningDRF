7ce6eeb42009aa09e6e8607bd822563aa0126d20
cHILLPill420 <ankitraya.ar@outlook.com>
2021-11-13 17:09:28 +0545

    commented validators

--- api/serializers.py
+++ api/serializers.py
@@ -5,34 +5,34 @@ from .models import Student
 
 class StudentSerializers(serializers.ModelSerializer):
     #Validators highest priority
-    def starts_with_a(value):
-        if value[0].lower() != 'a':
-            raise serializers.ValidationError('start with A pls')
+    # def starts_with_a(value):
+    #     if value[0].lower() != 'a':
+    #         raise serializers.ValidationError('start with A pls')
             
-    city = serializers.CharField(max_length=100, validators = [starts_with_a])
+    #city = serializers.CharField(max_length=100, validators = [starts_with_a])
     #roll = serializers.IntegerField(read_only = True) #doesn't let update roll
     class Meta:
         model = Student
-        fields = ['roll', 'city', 'name'] 
+        fields = ['id', 'roll', 'city', 'name'] 
         #read_only_fileds = ['roll', 'name'] #making multiple fields read only
         #extra_kwargs = {'name':{'read_only':True}}#can use any kinds of core arguements
     
-    # Field level Validators .is_valid() invokes this kind of validators -2nd highest priority
-    def validate_roll(self, value):
-        if value>=1000:
-            raise serializers.ValidationError('Seat Full')
-        return value
+    # Field level Validators |.is_valid() invokes this kind of validators -2nd highest priority
+    # def validate_roll(self, value):
+    #     if value>=1000:
+    #         raise serializers.ValidationError('Seat Full')
+    #     return value
     
     #object level validators ie multiple field validators- lowest priority
-    def validate(self, attrs): #here attrs is a python dictionary of fields values
-        nm = attrs.get('name')
-        rl = attrs.get('roll')
-        ct = attrs.get('city')
-        if rl>=1000:
-            raise serializers.ValidationError('No such rolls')
-        if nm[0] != 'A' and ct[0] != 'A':
-            raise serializers.ValidationError('A chaiyeko thyo regex feri herna parne vayo')
-        return attrs
+    # def validate(self, attrs): #here attrs is a python dictionary of fields values
+    #     nm = attrs.get('name')
+    #     rl = attrs.get('roll')
+    #     ct = attrs.get('city')
+    #     if rl>=1000:
+    #         raise serializers.ValidationError('No such rolls')
+    #     if nm[0] != 'A' and ct[0] != 'A':
+    #         raise serializers.ValidationError('A chaiyeko thyo regex feri herna parne vayo')
+    #     return attrs
          
 # class StudentSerializers(serializers.Serializer):
 #     id = serializers.IntegerField()
